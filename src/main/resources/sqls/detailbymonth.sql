e6d2981868690d9901284308b204aa58a71c9555
NewSingle <Rzeng_15094091094@163.com>
2016-07-06 10:14:21 +0800

    参数修改

--- src/main/resources/sqls/detailbymonth.sql
+++ src/main/resources/sqls/detailbymonth.sql
@@ -1,16 +1,16 @@
-//各地区日现金气量核对
-//month：月份,districtname:小区名称
+//各地区月明细表
+//time:查询时间,districtname:小区名称,usertype:用户类型,condition:查询条件
 //return：日期，居民气量，实际金额，户数，用户气量单价
 select 
 	SUBSTRING(CONVERT(varchar,f_deliverydate,120),1,11) as f_deliverydata,
 	SUM(f_pregas) as f_pregas,f_stair1price,SUM(f_preamount) as f_preamount,
-	COUNT(*) as c,f_gasprice
+	COUNT(*) as c
 from
 	t_sellinggas
 where 
-	f_deliverydate>='2016-0{month}-01' and f_deliverydate<='2016-0{month}-30'
-	and f_districtname like '{districtname}%' 
+	SUBSTRING(CONVERT(varchar,f_deliverydate,120),1,7) = '{time}' --输入月份，如‘2016-03’
+	and f_districtname like '{districtname}%'{condition}
 group by
-	SUBSTRING(CONVERT(varchar,f_deliverydate,120),1,11),f_gasprice,f_stair1price
+	SUBSTRING(CONVERT(varchar,f_deliverydate,120),1,11),f_stair1price
 order by 
 	SUBSTRING(CONVERT(varchar,f_deliverydate,120),1,11)
