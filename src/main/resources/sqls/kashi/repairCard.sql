5ce9f391469895f28d4bf582bc7bec025cb04c79
NewSingle <Administrator@PC201607121634>
2016-08-09 16:30:46 +0800

    修改参数

--- src/main/resources/sqls/kashi/repairCard.sql
+++ src/main/resources/sqls/kashi/repairCard.sql
@@ -1,7 +1,7 @@
 //补卡查询
 //营业网点:network,操作员:operator,开始时间:startDate,截止时间:endDate,orderitem:需要排序的字段
-//return:补卡数(c),补卡金额(f_cardfees),处理卡次数(f_times)
-select COUNT(*) c,SUM(f_cardfees) f_cardfees,count(f_times) f_times
+//return:补卡数(c),补卡金额(f_cardfees)
+select COUNT(*) c,SUM(f_cardfees) f_cardfees
 from t_renewalcard 
 where f_payfeevalid='有效' and f_parentname = '{network}' and f_rcoperator = '{operator}' 
 	  and f_recoverycarddate >= '{startDate}' and f_recoverycarddate <='{endDate}'
