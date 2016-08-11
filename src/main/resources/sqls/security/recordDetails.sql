9a9d8920ca0508a8c45bdf13e671abdf8184e3af
NewSingle <Administrator@PC201607121634>
2016-08-11 11:11:51 +0800

    安检修改

--- src/main/resources/sqls/security/recordDetails.sql
+++ src/main/resources/sqls/security/recordDetails.sql
@@ -1,9 +1,8 @@
 //查询安检记录详情
-//condition:查询条件
+//condition:查询条件,orderitem:需要排序的字段
 //return:重复的取最新的一条安检记录  
-
- select * FROM (select * from T_INSPECTION where {condition}  ) i
+ select * FROM (select * from T_INSPECTION ) i
  join 
- (select MAX(id) ids,F_CONSUMERNAME name from T_INSPECTION group by F_CONSUMERNAME,id) n 
+ (select MAX(id) ids,F_CONSUMERNAME name from T_INSPECTION where {condition} group by F_CONSUMERNAME) n 
  on  n.name = i.F_CONSUMERNAME and i.id=n.ids
- order by id
+order by {orderitem}
